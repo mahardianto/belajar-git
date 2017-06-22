15a81911f05ebc7e014053db07f212afb13297c7
Himawan Mahardianto <mahardianto@gmail.com>
2017-06-22 14:07:54 +0700

    menghapus folder apps/

--- apps/resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-  <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 1
-    </div>
-    <strong>Copyright &copy; 2017 <a href="http://mpn.kominfo.go.id">Monumen Pers Nasional</a>.</strong> All rights
-    reserved.
-  </footer>
